af3b5742dd66f6236631991182cd185fbeefbdc3
Rogach <platon7pronko@gmail.com>
2013-10-08 20:27:42 +0400

    Updated ./upd.sh

--- upd.sh
+++ upd.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
-grunt build compress
+grunt less build compress
+sleep 0.5
 tar -xvzf tmp/kibana-latest.tar.gz -C /var/www/
-sed -i 's/elasticsearch:.*/elasticsearch: "http:\/\/parrot-log.rogach.org",/' /var/www/kibana-latest/config.js
+sed -i 's/elasticsearch:.*/elasticsearch: "http:\/\/parrot-log.rogach.org:9201",/' /var/www/kibana-latest/config.js
